b0e32af0b85ca8ed49f6fead9cb91595c5ed82c4
pmountaindiver <121227608+pmountaindiver@users.noreply.github.com>
2022-12-25 16:21:20 +0100

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+import streamlit as st
+
+st.write('Hello world!')
+
+import streamlit as st
+
+st.header('Ciao Bello')
+
+if st.button('Say hello'):
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
+# In[ ]:
+
+
+
+
