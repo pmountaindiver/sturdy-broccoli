f586702f1cd351d9f981af6346f2de007fce679d
pmountaindiver <121227608+pmountaindiver@users.noreply.github.com>
2023-01-18 08:00:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,11 @@
 import streamlit as st
 import pandas as pd
 import base64
-def create_onedrive_directdownload (onedrive_link):
-    data_bytes64 = base64.b64encode(bytes(onedrive_link, 'utf-8'))
-    data_bytes64_String = data_bytes64.decode('utf-8').replace('/','_').replace('+','-').rstrip("=")
-    resultUrl = f"https://api.onedrive.com/v1.0/shares/u!{data_bytes64_String}/root/content"
-    return resultUrl
+#def create_onedrive_directdownload (onedrive_link):
+    #data_bytes64 = base64.b64encode(bytes(onedrive_link, 'utf-8'))
+    #data_bytes64_String = data_bytes64.decode('utf-8').replace('/','_').replace('+','-').rstrip("=")
+    #resultUrl = f"https://api.onedrive.com/v1.0/shares/u!{data_bytes64_String}/root/content"
+    #return resultUrl
 #fn = r"C:/Users/peh/OneDrive/1 Finance/2022 Finanz Analyse/Python/Data/Reference/entries.xlsx"
 onedrive_link=https://1drv.ms/x/s!AoEzYwQs3f9mkIQBeQNATkGFw3xcgw?e=7rbGfn
      
