084caab0f8d021b7dce1d438d59697c4045ef191
pmountaindiver <121227608+pmountaindiver@users.noreply.github.com>
2023-01-18 08:07:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,19 +4,20 @@
 # 
 import streamlit as st
 import pandas as pd
-#import base64
-#def create_onedrive_directdownload (onedrive_link):
-    #data_bytes64 = base64.b64encode(bytes(onedrive_link, 'utf-8'))
-    #data_bytes64_String = data_bytes64.decode('utf-8').replace('/','_').replace('+','-').rstrip("=")
-    #resultUrl = f"https://api.onedrive.com/v1.0/shares/u!{data_bytes64_String}/root/content"
-    #return resultUrl
+import base64
+def create_onedrive_directdownload (onedrive_link):
+    data_bytes64 = base64.b64encode(bytes(onedrive_link, 'utf-8'))
+    data_bytes64_String = data_bytes64.decode('utf-8').replace('/','_').replace('+','-').rstrip("=")
+    resultUrl = f"https://api.onedrive.com/v1.0/shares/u!{data_bytes64_String}/root/content"
+    return resultUrl
 #fn = r"C:/Users/peh/OneDrive/1 Finance/2022 Finanz Analyse/Python/Data/Reference/entries.xlsx"
 onedrive_link='https://1drv.ms/x/s!AoEzYwQs3f9mkIQBeQNATkGFw3xcgw?e=7rbGfn'
      
-x = pd.read_excel(onedrive_link)
+#x = pd.read_excel(onedrive_link)
+x=create_onedrive_directdownload (onedrive_link)
 st.write('Hello world!')
 st.write(onedrive_link)
-
+st.write(x)
 st.header('Ciao Bello')
 
 if st.button('Say hello'):
